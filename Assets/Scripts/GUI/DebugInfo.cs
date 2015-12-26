b7144cffb7fa2f29db3d7f39f7c7c1edf3bce036
Piotr Merski <merskip@gmail.com>
2015-12-26 14:40:55 +0100

    Dodanie panelu z informacjami o świecie gry

---
+++ Assets/Scripts/GUI/DebugInfo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class DebugInfo : MonoBehaviour {
+
+    private TerrainCreator terrainCreator;
+
+    private string format;
+    private Text text;
+
+    private void Awake() {
+        text = GetComponent<Text>();
+        format = text.text;
+
+        terrainCreator = FindObjectOfType<TerrainCreator>();
+        if (terrainCreator == null || !terrainCreator.enabled) {
+            Debug.LogError("Not found TerrainCreator or is enabled on scene!");
+            enabled = false;
+        }
+    }
+	
+	void LateUpdate() {
+        var coords = terrainCreator.CurrentCoords;
+        var chunks = terrainCreator.TerrainManager.getChunkMap().Count;
+        text.text = string.Format(format, coords.x, coords.y, chunks);
+	}
+}
